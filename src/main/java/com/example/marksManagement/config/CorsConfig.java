43730ef37f96740b2b2b096bb31081526315b56d
Venkataprasad20 <venkatrc930@gmail.com>
2026-01-11 00:08:32 +0530

    added url path

--- src/main/java/com/example/marksManagement/config/CorsConfig.java
+++ src/main/java/com/example/marksManagement/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                        		"https://marks-management-frontend-tqh5.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
